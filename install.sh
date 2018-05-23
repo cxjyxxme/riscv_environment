2ed3ea161dca98e8e40d4799c67515cdb6cf7ec5
杨国烨 <yanggy15@mails.tsinghua.edu.cn>
2018-05-23 19:57:53 +0800

    fix bug

--- install.sh
+++ install.sh
@@ -2,9 +2,4 @@ wget -q -O riscv.zip https://cloud.tsinghua.edu.cn/f/2cbf7164762c4e86993c/?dl=1
 unzip -q riscv.zip
 ls
 pwd
-export RISCV="/riscv/toolchain"
-export PATH=$RISCV/bin:$PATH
-echo $RISCV
-echo $PATH
 mv /ucore/mk/mtrap.c /riscv-pk/machine/mtrap.c
-ls $RISCV/bin
