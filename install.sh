71c2e2ce7b2f86ecbda4ff3dedead482e48f5966
杨国烨 <yanggy15@mails.tsinghua.edu.cn>
2018-05-24 10:47:44 +0800

    autotest for riscv

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 wget -q -O riscv.zip https://cloud.tsinghua.edu.cn/f/2cbf7164762c4e86993c/?dl=1 
-wget -q -O libs.zip https://cloud.tsinghua.edu.cn/f/36b52390b7d244f6a1b8/?dl=1
+wget -q -O libs.zip 
 unzip -q riscv.zip
 unzip -q libs.zip
 #cp -vnpr /libs/libgcrypt* /usr/lib/x86_64-linux-gnu/
-awk 'BEGIN { cmd="cp -i /libs/* /usr/lib/x86_64-linux-gnu/"; print "n" |cmd; }'
+cp -vnpr /libs/* /usr/lib/x86_64-linux-gnu/
+#awk 'BEGIN { cmd="cp -i /libs/* /usr/lib/x86_64-linux-gnu/"; print "n" |cmd; }'
 ln -s -T /usr/lib/x86_64-linux-gnu/libgcrypt.so /usr/lib/x86_64-linux-gnu/libgcrypt.so.11
 ls /usr/lib/x86_64-linux-gnu/
 pwd
