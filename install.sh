065f0730c382d736952992926dd79052fe4a313a
杨国烨 <yanggy15@mails.tsinghua.edu.cn>
2018-05-24 09:21:22 +0800

    autotest for riscv

--- install.sh
+++ install.sh
@@ -3,6 +3,7 @@ wget -q -O libs.zip https://cloud.tsinghua.edu.cn/f/36b52390b7d244f6a1b8/?dl=1
 unzip -q riscv.zip
 unzip -q libs.zip
 ls libs
-cp /libs/libgcrypt* /usr/lib/x86_64-linux-gnu/
+#cp /libs/libgcrypt* /usr/lib/x86_64-linux-gnu/
+cp /libs/* /usr/lib/x86_64-linux-gnu/
 pwd
 mv /ucore/mk/mtrap.c /riscv-pk/machine/mtrap.c
